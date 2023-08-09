456b106724c27690076f37c2a6740d94be66cd93
BSHathcock <blakehathcock04@gmail.com>
2023-08-08 20:17:10 -0600

    Made postMessage and added some code

---
+++ front/Scripts/postMessage.js
@@ -0,0 +1,22 @@
+// The user and message will have to come from elsewhere
+let time = null
+let date = null
+
+let messageData = {
+    mTime : null,
+    mDate : null,
+    mMessage : null,
+    mUsername : null
+}
+
+const getTimeAndDate = () => {
+    let dateTime = new Date()
+    
+
+    
+}
+
+const validateMessage = () => {
+    getTimeAndDate()
+
+}
