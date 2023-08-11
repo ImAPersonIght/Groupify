54357f5298a6765b5a85f552c8479d2d0679cf0e
BSHathcock <blakehathcock04@gmail.com>
2023-08-10 20:08:38 -0600

    bug fix

--- front/Scripts/login.js
+++ front/Scripts/login.js
@@ -1,27 +1,25 @@
 
 
 document.getElementById('login-button').addEventListener('click', function(event) {
-    event.preventDefault(); 
+    event.preventDefault()
   
-    const username = document.getElementById('login-email').value;
+    const username = document.getElementById('login-email').value
   
-    // Make a GET request to fetch user data by username
     fetch(`/username/${username}`)
       .then(response => response.json())
       .then(user => {
-        const passwordInput = document.getElementById('login-password').value;
+        const passwordInput = document.getElementById('login-password').value
   
        
         if (user.password === passwordInput) {
-          console.log('Login successful');
-          // Redirect or update UI as needed
+          console.log('Login successful')
+          //Go to the main page after they login
         } else {
-          console.error('Login failed: Password is incorrect');
+          console.error('Login failed: Password is incorrect')
           
         }
       })
       .catch(error => {
-        console.error('Error:', error);
-        // Handle error
+        console.error('Error:', error)
       });
   });
