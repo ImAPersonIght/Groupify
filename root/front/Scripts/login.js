a774adede2b5c413cd99c95cdf1adc52f5927c48
ImAPersonIght <zomhorsethompson2656@gmail.com>
2023-08-15 18:40:34 -0600

    made minor changes to login.js

--- root/front/Scripts/login.js
+++ root/front/Scripts/login.js
@@ -1,19 +1,22 @@
-var url = "http://localhost:2718"
+    var url = "http://localhost:2718/user"
 
-document.getElementById('login-button').addEventListener('click', function(event) { // need to connect to the login button
-    event.preventDefault()
-    const email = document.getElementById('login-email').value // needs to connect to the email text box
-    fetch(`${url}/email/${email}`)
-        .then(response => response.json())
-        .then(data => {
-        const passwordInput = document.getElementById('login-password').value // need to connect this to the password text box
-        if (data.password === passwordInput) {
-            console.log('Login successful') // Put the page right here with the account assigned to the user
-        } else {
-            console.error('Login failed: Password is incorrect') // have them try again
-        }
+    console.log("this is connected")
+
+    document.getElementById('login-button').addEventListener('click', function(event) {
+        console.log("button clicked")
+        event.preventDefault()
+        const email = document.getElementById('login-email').value
+        fetch(`${url}/email/${email}`)
+            .then(response => response.json())
+            .then(data => {
+                const passwordInput = document.getElementById('login-password').value
+                if (data.password === passwordInput) {
+                    console.log('Login successful')
+                } else {
+                    console.error('Login failed: Password is incorrect')
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error)
+            })
     })
-    .catch(error => {
-        console.error('Error:', error)
-    })
-})
