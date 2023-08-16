4c948dbbef1cbe06131eef84a72c4388dbfc4deb
BSHathcock <blakehathcock04@gmail.com>
2023-08-15 18:17:44 -0600

    login script done

--- root/front/Scripts/login.js
+++ root/front/Scripts/login.js
@@ -1,16 +1,16 @@
 var url = "http://localhost:2718"
 
-document.getElementById('login-button').addEventListener('click', function(event) {
+document.getElementById('login-button').addEventListener('click', function(event) { // need to connect to the login button
     event.preventDefault()
-    const username = document.getElementById('login-email').value
-    fetch(`${url}/username/${username}`)
+    const email = document.getElementById('login-email').value // needs to connect to the email text box
+    fetch(`${url}/email/${email}`)
         .then(response => response.json())
-        .then(user => {
-        const passwordInput = document.getElementById('login-password').value
-        if (user.password === passwordInput) {
-            console.log('Login successful')
+        .then(data => {
+        const passwordInput = document.getElementById('login-password').value // need to connect this to the password text box
+        if (data.password === passwordInput) {
+            console.log('Login successful') // Put the page right here with the account assigned to the user
         } else {
-            console.error('Login failed: Password is incorrect')
+            console.error('Login failed: Password is incorrect') // have them try again
         }
     })
     .catch(error => {
