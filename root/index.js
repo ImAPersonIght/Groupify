4bbc4965d5cffec69fe50ba736e5e0ff2755abe1
ImAPersonIght <zomhorsethompson2656@gmail.com>
2023-08-15 18:19:53 -0600

    added rout to home in api

--- root/index.js
+++ root/index.js
@@ -15,6 +15,10 @@ app.get('/createuser', (req, res) => {
     res.sendFile(path.resolve(".\\front\\createUser.html"))
 })
 
+app.get('/home', (req, res)=>{
+    res.sendFile(path.resolve(".\\front\\messages.html"))
+})
+
 app.get('/', (req, res)=>{
     res.send('This is the Groupify api')
 })  
