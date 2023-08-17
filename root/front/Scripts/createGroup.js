37831bb4bfe7cc93cf0309f6a902604929bf249a
BSHathcock <blakehathcock04@gmail.com>
2023-08-16 19:07:33 -0600

    started room creation

---
+++ root/front/Scripts/createGroup.js
@@ -0,0 +1,18 @@
+
+document.getElementById('modal-create-group-btn').addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let name = document.getElementById('group-name').value
+    let topic = document.getElementById('group-name').value
+    let description = document.getElementById('group-name').value
+    let rules = document.getElementById('group-name').value
+})
+
+const createGroup = (name, topic, description, rules) => {
+    if (name === "") {
+        console.log("Please make sure name is filled")
+    } else {
+        
+    }
+}
+
