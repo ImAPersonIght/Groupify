0f5156b2330dece94bfac2594e339da7e904b4b3
BSHathcock <blakehathcock04@gmail.com>
2023-08-23 18:44:32 -0600

    L

--- root/front/Scripts/displayMessage.js
+++ root/front/Scripts/displayMessage.js
@@ -1,6 +1,8 @@
 
 function enterMessage() {
     const messageInput = document.getElementById("message-input").value
+    validateMessage(messageInput)
+
     var jsonInfo = getUserDataForMessage();
 
     var userName = jsonInfo.username
@@ -48,4 +50,84 @@ const getUserDataForMessage = async ()=>{
         return userData.json()
     }
     return null
+}
+
+// ------------------------------------
+
+
+const getDate = () => {
+    let date = new Date()
+
+    mDate = date.getDate() + "/"
+    + (date.getMonth()+1)  + "/" 
+    + date.getFullYear();
+
+    return mDate
+}
+
+const getTime = () => {
+    let time = new Date()
+
+    mTime = time.getHours() + ":"  
+    + time.getMinutes() + ":" 
+    + time.getSeconds();
+
+    return mTime
+}
+
+const validateMessage = (message) => {
+    const messGex = /^(.){1,300}$/
+
+    if(messGex.test(message)){
+        postMessage(message)
+    } else {
+        console.log("Message lenght is over 300")
+    }
+}
+
+const postMessage = async (mess) => {
+    const userData = await getUserDataForMessage();
+    const id = await getMessageID()
+    const url = "http://localhost:2718/message"
+    let messageData = {
+        messageID : id,
+        mTime : getTime(),
+        mDate : getDate(),
+        mMessage : mess,
+        accountToken : userData.account_token,
+        posterUsername : userData.username,
+        roomID : 1 // Need to find a way to get roomID
+    }
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(messageData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    });
+}
+
+const getMessageID = async () => {
+    let id = 1
+    try{
+        const response = await fetch(`http://localhost:2718/message/id/${id}`)
+        if(response.status === 404){
+            return id
+        }
+        else{
+            id++
+            getMessageId()
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
 }
