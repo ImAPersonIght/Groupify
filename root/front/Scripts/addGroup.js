8bd7a397cc223fc001c2cb3c5d85fb93926b08fa
AlyssaRippel <Arippel@Student.neumont.edu>
2023-08-23 19:59:16 -0600

    fixed groups https

--- root/front/Scripts/addGroup.js
+++ root/front/Scripts/addGroup.js
@@ -9,14 +9,14 @@ async function searchGroup(){
     const searchInput = document.getElementById("search-input").value
 
     //get the rooms info
-    const response = await fetch(`localhost:2718/room/name/${searchInput}`)
+    const response = await fetch(`http://localhost:2718/room/name/${searchInput}`)
     const data = await response.json()
 
     for(i = 0; i < data.length; i++){
         var currentGroup = data[i];
 
         const addGroupDiv = document.createElement("div");
-        addGroupDiv.setAttribute(`id", "group-add-${currentGroup}`)
+        addGroupDiv.setAttribute(`id, group-add-${currentGroup}`)
         addGroupDiv.onclick = async function(){
             //show a are you sure popup
         }
