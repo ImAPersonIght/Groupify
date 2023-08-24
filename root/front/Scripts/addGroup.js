46e0d472e0b244b3903e3fa4205e0bfbb8726854
ImAPersonIght <zomhorsethompson2656@gmail.com>
2023-08-23 19:58:36 -0600

    added http to urls

--- root/front/Scripts/addGroup.js
+++ root/front/Scripts/addGroup.js
@@ -9,7 +9,7 @@ async function searchGroup(){
     const searchInput = document.getElementById("search-input").value
 
     //get the rooms info
-    const response = await fetch(`localhost:2718/room/name/${searchInput}`)
+    const response = await fetch(`http://localhost:2718/room/name/${searchInput}`)
     const data = await response.json()
 
     for(i = 0; i < data.length; i++){
