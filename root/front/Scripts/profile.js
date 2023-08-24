9c9ae302fe7355c784d1860bde40696017a0c9bf
malloryguest09 <malz.guest@gmail.com>
2023-08-23 18:53:46 -0600

    profile css fix

---
+++ root/front/Scripts/profile.js
@@ -0,0 +1,17 @@
+// Display the Username and email of the current user from the database
+
+
+
+
+// Be able to take the friends of the current user and display them in a list 
+
+
+
+
+// Be able to search through all users in the database and be able to add them as friends or add them to a group
+
+
+
+
+
+
